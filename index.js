fff075d457701e913b16f88c95ddee19c3925f92
Fran Borg <francesca.bte@outlook.com>
2019-09-16 18:12:14 +0200

    Add timer

--- index.js
+++ index.js
@@ -3,18 +3,29 @@ const inputTime = document.getElementById("time");
 const startButton = document.getElementById("btn-start");
 const timerDisplay = document.getElementById("timer");
 
+
 session.addEventListener("click", (e) => {
-    switch(e.target.id) {
+    switch (e.target.id) {
         case "btn-plus":
-        inputTime.value++;
-        break;
+            inputTime.value++;
+            break;
         case "btn-minus":
-        inputTime.value--;
-        break;
+            inputTime.value--;
+            break;
     }
 });
 
+function startTimer() {
+    let timeLeft = inputTime.value * 60;
+    setInterval(() => {
+        timeLeft--;
+        document.querySelector(".mins").textContent = `${Math.floor(timeLeft / 60)}`;
+        document.querySelector(".secs").textContent = `${timeLeft % 60}`;
+    }, 1000)
+}
 
 
 
-
+startButton.addEventListener("click", () => {
+    startTimer();
+})
