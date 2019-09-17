87c20252cb4b81d9f851e1e059157c57a50710c3
Fran Borg <francesca.bte@outlook.com>
2019-09-17 11:21:28 +0200

    Promisify setInterval

--- index.js
+++ index.js
@@ -3,20 +3,42 @@ const buttons = document.querySelector(".buttons");
 const minutes = document.querySelector(".mins");
 const seconds = document.querySelector(".secs");
 
+// let ticking;
+// function startTimer() {
+//     //gets time in seconds
+//     let timeLeft = (Number(minutes.textContent) * 60) + Number(seconds.textContent);
+//     //begins countdown every second
+//     ticking = setInterval(() => {
+//         timeLeft--;
+//         //gets time left in minutes and seconds
+//         minutes.textContent = `${(Math.floor(timeLeft / 60))}`;
+//         seconds.textContent = `${timeLeft % 60}`;    
+//         if (timeLeft <= 0) {
+//             stopTimer(ticking);
+//         }
+//     }, 1000);
+// }
+
 let ticking;
+let timeLeft;
 function startTimer() {
     //gets time in seconds
-    let timeLeft = (Number(minutes.textContent) * 60) + Number(seconds.textContent);
+    timeLeft = (Number(minutes.textContent) * 60) + Number(seconds.textContent);
     //begins countdown every second
-    ticking = setInterval(() => {
-        timeLeft--;
-        //gets time left in minutes and seconds
-        minutes.textContent = `${(Math.floor(timeLeft / 60))}`;
-        seconds.textContent = `${timeLeft % 60}`;    
-        if (timeLeft <= 0) {
-            stopTimer(ticking);
-        }
-    }, 1000);
+    let startTicking = new Promise(function (resolve) {
+        resolve(timeLeft);
+    })
+    startTicking.then(function (remainder) {
+        ticking = setInterval(() => {
+            remainder--;
+            //gets time left in minutes and seconds
+            minutes.textContent = `${(Math.floor(remainder / 60))}`;
+            seconds.textContent = `${remainder % 60}`;
+            if (remainder <= 0) {
+                stopTimer(ticking);
+            }
+        }, 1000)
+    })
 }
 
 function stopTimer(timer) {
@@ -54,14 +76,14 @@ document.getElementById("session").addEventListener("click", (e) => {
 buttons.addEventListener("click", (e) => {
     switch (e.target.id) {
         case "btn-start":
-        //resets timer if it is already running
+            //resets timer if it is already running
             stopTimer(ticking);
             //checks if input is larger than 1 minute to include minutes
             if (inputTime.value >= 1) {
                 minutes.textContent = Math.floor(inputTime.value);
                 seconds.textContent = getDecimalPlaces(inputTime.value);
                 startTimer();
-            } 
+            }
             //checks if input is smaller than 1 minute to only include seconds if greater than 0
             else if ((inputTime.value < 1) && (inputTime.value > 0)) {
                 seconds.textContent = getDecimalPlaces(inputTime.value);
