db2442ad5388e788dee1f2a716a233cb388a159c
Fran Borg <francesca.bte@outlook.com>
2019-09-17 15:31:22 +0200

    Add functionality of modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,27 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import "../styles/main.scss";
 // Import any additional modules you want to include below \/
 
 
 // \/ All of your javascript should go here \/
 
+const modal = document.getElementById("myModal");
+let promiseOfModal = new Promise(function (resolve) {
+    window.setTimeout(function () {
+        resolve(modal)
+    }, (1000 * 60));
+});
+
+promiseOfModal.then(function(val) {
+    console.log("User has been on the page for 60 seconds");
+    val.style.display = "block";
+})
+
+modal.addEventListener("click", (e) => {
+    switch(e.target.className) {
+        case "close":
+        case "modal":
+        modal.style.display = "none"
+        break;
+    }
+})
